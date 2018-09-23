19a786c538c825ddda1e24ea446d11b9e8d88c84
Alais <10176830+Upl098@users.noreply.github.com>
2018-09-24 00:29:52 +0200

    updated to version 0.2-3

--- nspx.py
+++ nspx.py
@@ -4,132 +4,23 @@
     Copyright (c) 2018, Alais Mono
     Everyone is permitted to copy and distribute verbatim copies of this license document
 
-
-    - not the best code I've ever written, but it does the job
     - p.s.: python2/3 both seem to work
 """
 
 import sys, os
 import optparse
-from struct import pack as pk, unpack as upk
 from shutil import rmtree
+from pfs0 import PFS0File
 
 # exit/error codes
 SUCCESS       = 0
 NO_FILENAME   = 1
 NO_SUCH_FILE  = 2
 NO_ACTION     = 3
-INVALID_MAGIC = 4
-NO_INFILES    = 5
-UNIMPLEMENTED = 6
+NO_INFILES    = 4
+ERROR         = 255
 
 TMPDIR = os.path.abspath( ".nspx.tmp" )
-
-def gen_header(argc, argv):
-    stringTable = '\x00'.join([os.path.basename(file) for file in argv[1:]])
-    headerSize = 0x10 + (argc-1)*0x18 + len(stringTable)
-        #            0x10 = 16            = PFS0 (4bytes) + numFiles (4bytes) + str_table_len (4bytes) + unused (4bytes)
-        # numFiles * 0x18 = numFiles * 24 = data_offset (8bytes) + data_size (8bytes) + str_offset (4bytes) + unused (4bytes)
-    remainder = 0x10 - headerSize%0x10
-    headerSize += remainder
-    
-    fileSizes = [os.path.getsize(file) for file in argv[1:]]
-    fileOffsets = [sum(fileSizes[:n]) for n in range(argc-1)]
-    
-    fileNamesLengths = [len(os.path.basename(file))+1 for file in argv[1:]] # +1 for the \x00 separator
-    stringTableOffsets = [sum(fileNamesLengths[:n]) for n in range(argc-1)]
-    
-    header =  b''
-    header += b'PFS0'
-    header += pk('<I', argc-1)
-    header += pk('<I', len(stringTable)+remainder)
-    header += b'\x00\x00\x00\x00'
-    
-    for n in range(argc-1):
-        header += pk('<Q', fileOffsets[n])
-        header += pk('<Q', fileSizes[n])
-        header += pk('<I', stringTableOffsets[n])
-        header += b'\x00\x00\x00\x00'
-    header += stringTable.encode()
-    header += remainder * b'\x00'
-    
-    return header
-
-def mk_nsp( options, args ):
-    global TMPDIR
-    fname = options.filename
-
-    if os.path.isfile( fname ):
-        print( "Not supported, yet." )
-        os._exit( UNIMPLEMENTED )
-    else:
-        # create new archive with files
-        if not options.silent: print( "Creating new archive '%s'" % fname )
-        nspFile = open( os.path.abspath( fname ), "wb" )
-        nspFile.seek( 0 )
-
-        if not options.silent: print( "Generating header..." )
-        nspFile.write( gen_header( len( args ) + 1, [ "buffer" ] + args ) )
-
-        for name in args:
-            if not options.silent: print( "Appending '%s'" % os.path.basename( name ) )
-            with open( os.path.abspath( name ), "rb" ) as f:
-                while 1:
-                    chunk = f.read( 4096 )
-                    if chunk == b'': break
-                    nspFile.write( chunk )
-        
-        nspFile.close() 
-
-    if not options.silent: print( "Done!" )
-    os._exit( SUCCESS )
-
-
-def read_fspecs( fp, base, numFiles ):
-    fspecs = []
-
-    fp.seek( base )
-
-    for n in range( numFiles ):
-        data_offset = upk( "<Q", fp.read( 8 ) )[ 0 ]
-        data_size   = upk( "<Q", fp.read( 8 ) )[ 0 ]
-        name_offset = upk( "<I", fp.read( 4 ) )[ 0 ]
-
-        fspecs.append( ( data_offset, data_size, name_offset ) )
-
-        fp.read( 4 )
-    
-    return fspecs
-
-def read_filename( fp, offset ):
-    fp.seek( offset )
-
-    fname = b''
-
-    while 1:
-        b = fp.read( 1 )
-        if b == b'\x00': break
-        fname += b
-    
-    return fname.decode()
-
-def extract_file( fp_in, outfile, base, length ):
-    fp_in.seek( base )
-
-    goal = base + length
-
-    fp_out = open( outfile, "wb" )
-
-    while 1:
-        rest = goal - fp_in.tell()
-
-        if rest < 4096:
-            fp_out.write( fp_in.read( rest ) )
-            break
-        else:
-            fp_out.write( fp_in.read( 4096 ) )
-    
-    fp_out.close()
 
 def main():
     parser = optparse.OptionParser( "usage: %prog [options] file1 file2 ..." )
@@ -152,7 +43,7 @@ def main():
     ( options, args ) = parser.parse_args()
 
     if options.action == 4:
-        print( "nspx (NSP eXtractor) v0.2-2" )
+        print( "nspx (NSP eXtractor) v0.2-3" )
         os._exit( SUCCESS )
     elif options.filename == None:
         print( "Need to specify a filename with '-f'" )
@@ -160,6 +51,8 @@ def main():
     elif not os.path.isfile( options.filename ) and options.action != 3:
         print( "There is no such file '%s'" % options.filename )
         os._exit( NO_SUCH_FILE )
+
+    if not options.silent: PFS0File.set_logger( lambda msg: sys.stdout.write( '%s\n' % msg ) )
 
     if options.action == 0:
         print( "Nothing to do" )
@@ -174,75 +67,37 @@ def main():
                 print( "No such file '%s'" % name )
                 os._exit( NO_SUCH_FILE )
 
-        mk_nsp( options, args )
+        PFS0File.create_pfs0( options.filename, args ).close()
         os._exit( SUCCESS )
     
-    fspecs_base = 0 # base of fspecs
-    stable_base = 0 # base of string table
-    body_base   = 0 # base of body
+    try:
+        nspFile = PFS0File( open( options.filename, "rb" ) )
 
-    nspFile = open( os.path.abspath( options.filename ), "rb" )
-    nspFile.seek( 0 )
-
-    if nspFile.read( 4 ) != b'PFS0':
-        print( "Invalid file magic." )
-        os._exit( INVALID_MAGIC )
-
-    numFiles   = upk( "<I", nspFile.read( 4 ) )[ 0 ] # number of files in archive
-    stableSize = upk( "<I", nspFile.read( 4 ) )[ 0 ] # size of string table
-
-    nspFile.read( 4 ) # skip seperator
-
-    fspecs_base = nspFile.tell()
-
-    fspecs = read_fspecs( nspFile, fspecs_base, numFiles )
-
-    stable_base = nspFile.tell()
-    body_base   = stable_base + stableSize
-
-    if options.action == 2:
-        if ret_results != True: print( "Files in '%s':" % options.filename )
-        
-        # store info about name, size for pretty printing
-        fnames, fsizes = [], []
-
-        for fspec in fspecs:
-            fnames.append( read_filename( nspFile, stable_base + fspec[ 2 ] ) )
-            fsizes.append( str( fspec[ 1 ] ) )
-
-        if ret_results == True:
-            return nspFile, fspecs, fnames
-
-        maxName = len( max( fnames, key=len ) )
-        maxSize = len( max( fsizes, key=len ) )
-        
-        for n in range( len( fnames ) ):
-            print( "\t%s : %s bytes" % ( fnames[ n ].ljust( maxName ), fsizes[ n ].rjust( maxSize ) ) )
-        
-    elif options.action == 1:
-        outdir = os.path.abspath( options.outdir ) if options.outdir != None else os.path.abspath( os.path.splitext( options.filename )[ 0 ] )
-        if not os.path.isdir( outdir ):
-            if not options.silent: print( "Creating output directory '%s'" % outdir )
-            os.mkdir( outdir )
-
-        if len( args ) > 0:
-            for fspec in fspecs:
-                fname = read_filename( nspFile, stable_base + fspec[ 2 ] )
-                if fname in args:
-                    if not options.silent: print( "Extracting file '%s'" % fname )
-                    extract_file( nspFile, os.path.join( os.path.abspath( "." ), outdir, fname ), body_base + fspec[ 0 ], fspec[ 1 ] )
-        else:
-            for fspec in fspecs:
-                fname = read_filename( nspFile, stable_base + fspec[ 2 ] )
-                if not options.silent: print( "Extracting file '%s'" % fname )
-                extract_file( nspFile, os.path.join( os.path.abspath( "." ), outdir, fname ), body_base + fspec[ 0 ], fspec[ 1 ] )
+        if options.action == 2:
+            print( "Files in '%s':" % options.filename )
             
-        if not options.silent: print( "Done!" )
-        if ret_results == True: return
+            flist = nspFile.listfiles()
 
-    nspFile.close()
+            fnames, fsizes = [ f[ 2 ] for f in flist ], [ str( f[ 1 ] ) for f in flist ]
 
-    os._exit( SUCCESS )
+            maxName = len( max( fnames, key=len ) )
+            maxSize = len( max( fsizes, key=len ) )
+            
+            for n in range( len( fnames ) ):
+                print( "\t%s : %s bytes" % ( fnames[ n ].ljust( maxName ), fsizes[ n ].rjust( maxSize ) ) )
+            
+        elif options.action == 1:
+
+            nspFile.extract_files( args, options.outdir or os.path.splitext( options.filename )[ 0 ] )
+
+            if not options.silent: print( "Done!" )
+
+        nspFile.close()
+
+        os._exit( SUCCESS )
+    except Exception as ex:
+        print( ex )
+        os._exit( ERROR )
 
 if __name__ == "__main__":
     if os.path.isdir( TMPDIR ):
